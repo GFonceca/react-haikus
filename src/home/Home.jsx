604d5f8deceec72410a419ad01c552e7e1730ef9
GustavoFonceca <juniorfonceca04@gmail.com>
2021-11-01 13:50:15 -0300

    denuevo

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -5,7 +5,7 @@ import Tarjeta from "./tarjeta";
 const Home = () => {
   return (
     <>
-      <h4 className="H4"><h4>Colección</h4></h4>
+      <h4 className="H4">Coleccion</h4>
       <div id="galeria">
         <Tarjeta />
         <Tarjeta />
