c9c9e2ec2391f63c0665a33c2279e3a9465aa02c
GFonceca <89805972+GFonceca@users.noreply.github.com>
2021-11-03 13:04:19 -0300

    actualizado 3/11

--- src/home/tarjeta.jsx
+++ src/home/tarjeta.jsx
@@ -7,12 +7,12 @@ const Tarjeta = () => {
     return (
       <div className="Tarjeta">
         <div className="ImagenTarjeta">
-          <img src={`https://picsum.photos/500/300`} alt="ImagenHaiku" />
+          <img  src={`https://picsum.photos/500/300`} alt="ImagenHaiku" />
         </div>
-        <div className="CuerpoTarjeta">
+        <div className="CuerpoDeTarjeta">
           <div className="InfoTarjeta">
-            <img src={`https://picsum.photos/32/32`} alt="PerfilHaiku" />
-            <h4>Titulo del Haiku</h4>
+            <img className="imagenPerfil" src={`https://picsum.photos/32/32`} alt="PerfilHaiku" />
+            <h4 className="H4">Titulo del Haiku</h4>
           </div>
           <FontAwesomeIcon icon={faShareAlt} className="IconoCompartirTarjeta" />
         </div>
