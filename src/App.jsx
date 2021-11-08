b45acddeef6231ab1478a997058b5ee65a583c55
GustavoFonceca <juniorfonceca04@gmail.com>
2021-11-08 14:27:02 -0300

    actualizado 8/11/21

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import Header from "./layout/Header";
 import Main from "./layout/Main";
 import Footer from "./layout/Footer";
 import Home from './home/Home';
+import Contactanos from "./contactos/Contactanos";
 
 const App = () => {
   return (
     <>
       <Header /> 
       <Main>
-        <Home />
+        {/* <Home /> */}
+        <Contactanos />
       </Main>
       <Footer />
     </>
